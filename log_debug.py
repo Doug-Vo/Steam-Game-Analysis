ad145f2f692fdc0df09d80c18d81df0ed9eebf00
Doug-Vo <ducthiennguyenvo@gmail.com>
2025-10-14 11:58:18 +0300

    Update MongoDB connection with log debug

---
+++ log_debug.py
@@ -0,0 +1,24 @@
+import colorama
+import os
+import logging
+from pymongo import MongoClient
+
+
+colorama.init(autoreset= True)
+class ColoredFormatter(logging.Formatter):
+    COLORS = {
+        'WARNING' : colorama.Fore.YELLOW, 
+        'INFO' : colorama.Fore.GREEN,
+        'ERROR': colorama.Fore.RED 
+}
+    
+    def format(self, record):
+        log_message = super().format(record)
+        return self.COLORS.get(record.levelname, '') + log_message
+    
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+if logger.hasHandlers(): logger.handlers.clear()
+handler = logging.StreamHandler()
+handler.setFormatter(ColoredFormatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger.addHandler(handler)
